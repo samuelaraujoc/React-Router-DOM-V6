1a2b9ed0f7bcac9aad639091554158a9fda4b9e2
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2023-06-13 20:28:23 -0300

    Config

---
+++ react-rout-dom/src/components/About/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const About = () => {
+    return(
+        <div>
+            <h1>Teste About</h1>
+        </div>
+    );
+};
--- react-rout-dom/src/main.tsx
+++ react-rout-dom/src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './styles/Global.css'
-import { Home } from './components/Home';
-
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
